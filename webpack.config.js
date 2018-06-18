ba53fab5b7055c3775fe72f6f7ab9a30e23bffaf
Nova <novka666@mail.ru>
2018-06-18 14:08:59 +0300

    Final Game

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	devtool: 'source-map',
+	entry: './js/script.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/dist/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(jpg|png)$/,
+				loader: 'file-loader?name=/fonts/[name].[ext]'
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file-loader?name=/fonts/[name].[ext]'
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader?url=false"
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: [path.resolve(__dirname, 'node_modules')],
+				use: [
+					{
+						loader: 'babel-loader'
+						//preserts: ["env"] 
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	]
+}
